c3e6b5ff802e417c131de2748ed4826dcfa13aac
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-02-24 10:24:26 +0100

    Add default form styles

---
+++ app/routes/form.tsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <div>
+      <label>
+        <span>Text Field</span>
+        <input type="text" name="text" id="text" className="block" />
+      </label>
+    </div>
+  );
+};
