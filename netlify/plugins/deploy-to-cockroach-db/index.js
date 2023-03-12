0fcf89718f3eb63706d02b4b175ff2d27693e992
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-03-12 23:11:31 +0100

    WIP: Deploy database via netlify plugin /3

--- netlify/plugins/deploy-to-cockroach-db/index.js
+++ netlify/plugins/deploy-to-cockroach-db/index.js
@@ -1,3 +1,9 @@
-export const onPreBuild = function () {
-  console.log("Hello world from onPreBuild event!");
+// docs:
+// - https://docs.netlify.com/integrations/build-plugins/create-plugins/#utilities
+// - https://github.com/netlify/build/blob/main/packages/run-utils/README.md
+
+export const onBuild = async function ({ utils: { build, run } }) {
+  console.log("before prisma deploy");
+  await run.command(`npx prisma migrate deploy`);
+  console.log("after prisma deploy");
 };
