ee115a8c4fc65f6c0062195429adda95dd3d5b31
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-04-04 00:06:53 +0200

    Deploy prisma schema before building remix

--- netlify/plugins/deploy-to-cockroach-db/index.js
+++ netlify/plugins/deploy-to-cockroach-db/index.js
@@ -2,7 +2,7 @@
 // - https://docs.netlify.com/integrations/build-plugins/create-plugins/#utilities
 // - https://github.com/netlify/build/blob/main/packages/run-utils/README.md
 
-export const onBuild = async function ({ utils: { build, run } }) {
+export const onPreBuild = async function ({ utils: { build, run } }) {
   // TODO error handling?
   await run.command(`npx prisma migrate deploy`);
 };
