94d9427da8353638436e97a3f40538993c49422c
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-04-04 00:04:11 +0200

    Deploy prisma schema without netlify plugin

--- netlify/plugins/deploy-to-cockroach-db/index.js
+++
@@ -1,8 +0,0 @@
-// docs:
-// - https://docs.netlify.com/integrations/build-plugins/create-plugins/#utilities
-// - https://github.com/netlify/build/blob/main/packages/run-utils/README.md
-
-export const onBuild = async function ({ utils: { build, run } }) {
-  // TODO error handling?
-  await run.command(`npx prisma migrate deploy`);
-};
