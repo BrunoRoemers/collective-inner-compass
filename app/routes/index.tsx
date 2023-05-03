8dd243c4f4cd02ffa33e684c472c1a71c1a703db
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-05-03 14:34:05 +0200

    Show login status on homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,50 @@
-import { Link } from "@remix-run/react";
+import { z } from "zod";
+import type { DataFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/session.server";
+
+const response = z.object({
+  user: z.union([
+    z.null(),
+    z.object({
+      id: z.string().uuid(),
+      name: z.string(),
+      email: z.string().email(),
+    }),
+  ]),
+});
+
+type Response = z.infer<typeof response>;
+
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const userId = await getUserId(request);
+
+  if (userId === undefined) {
+    return json<Response>({
+      user: null,
+    });
+  }
+
+  const user = await db.user.findUniqueOrThrow({
+    where: {
+      id: userId,
+    },
+  });
+
+  return json<Response>({
+    user: {
+      id: user.id,
+      name: user.name ?? "Anon",
+      email: user.email,
+    },
+  });
+};
 
 export default function Index() {
+  const { user } = response.parse(useLoaderData());
+
   return (
     <div>
       <ul>
@@ -10,7 +54,13 @@ export default function Index() {
           </Link>
         </li>
       </ul>
-      TODO login / logout here
+      <div>
+        (
+        {user === null
+          ? "not logged in"
+          : `logged in as: ${user.name} (${user.email})`}
+        )
+      </div>
     </div>
   );
 }
